0e326e9ff8b5e3e722f19ac3217829ad313c4e80
eileen.lai <eileen.lai@rafaelmicro.com>
2023-04-24 11:34:18 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Eileen on 2023/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
