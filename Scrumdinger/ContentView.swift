22ae5318cc1bf1844174665d10776281db9f5b09
eileen.lai <eileen.lai@rafaelmicro.com>
2023-04-24 11:55:37 +0800

    Supplement accessibility data

--- Scrumdinger/ContentView.swift
+++ Scrumdinger/ContentView.swift
@@ -24,13 +24,19 @@ struct ContentView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
-            Circle().strokeBorder(lineWidth: 24)
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
             HStack {
                 Text("Speaker 1 of 3")
                 Spacer()
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
