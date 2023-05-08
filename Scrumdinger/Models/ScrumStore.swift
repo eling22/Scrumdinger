d4a0f9ad2116e8d9bb2b9bfd5b46826c9ae60a24
eileen.lai <eileen.lai@rafaelmicro.com>
2023-05-08 15:59:56 +0800

    Create a data store

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Eileen on 2023/5/8.
+//
+
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+    }
+}
